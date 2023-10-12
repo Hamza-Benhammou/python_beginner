3d9b3f587d7852fda1d1de7c236b948cbec13209
Hamza Benhammou <haamzaa.be@gmail.com>
2023-10-12 22:56:30 +0200

    Comment afficher un message dans la console

---
+++ start_2.py
@@ -0,0 +1,7 @@
+# Comment afficher un message dans la console
+
+x = "Hello world!"
+print(x)
+
+# Déclarer une variable dans laquelle vous y affectez une valeur, ici il s'agit de la chaine de caractère "Hello world!"
+# Puis utilisez print entre parenthèses le nom de votre variable pour l'afficher dans la console
