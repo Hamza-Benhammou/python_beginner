9b3e08a41bc7bbc58f356efd65529d68b9396d49
Hamza Benhammou <haamzaa.be@gmail.com>
2023-10-12 22:42:13 +0200

    Découverte des différentes classes pour travailler sur python

---
+++ start_1.py
@@ -0,0 +1,19 @@
+# Pour commencer voyons les différentes classes qui existent sur python 
+
+    # int: Classe pour les entiers.(comme 1, 7, 34)
+    # float: Classe pour les nombres à virgule flottante. (comme 12.3, 3.56)
+    # str: Classe pour les chaînes de caractères. (comme "John", "Bonjour" ils doivent être entre "")
+    # list: Classe pour les listes, qui sont des séquences modifiables d'éléments. (Les listes sont entre [])
+    # tuple: Classe pour les tuples, qui sont des séquences immuables d'éléments.
+    # dict: Classe pour les dictionnaires, qui sont des structures de données associatives. (Ils sont entre {} généralement on y stocke des clés avec leurs valeurs)
+    # set: Classe pour les ensembles, qui sont des collections non ordonnées d'éléments uniques. (Ils sont entre {} )
+    # bool: Classe pour les valeurs booléennes (True ou False).
+    # NoneType: Classe pour l'objet None, qui représente l'absence de valeur.
+    # file: Classe pour les fichiers, utilisée pour la manipulation des fichiers.
+    # function: Les fonctions elles-mêmes sont des objets de la classe fonction.
+    # class: Les classes elles-mêmes sont des objets de la classe class. (Pour déterminer la classe d'un élément)
+    # module: Les modules Python sont des objets de la classe module.
+    # Exception: Classe de base pour les exceptions.
+    # object: Toutes les classes en Python héritent implicitement de la classe object.
+
+    # Il en existe d'autres mais celles-ci sont les plus utilisées
