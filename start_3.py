39abd53634f1e277c68c9e7ea968ce87b3b0bd62
Hamza Benhammou <haamzaa.be@gmail.com>
2023-10-15 15:35:27 +0200

    comment afficher un message en plus de la valeur d'une variable quand on print

---
+++ start_3.py
@@ -0,0 +1,9 @@
+# Comment afficher un message dans la console en plus de la valeur d'une variable
+
+nom = "Mohamed"
+print(f"Cet homme s'appelle {nom}")
+
+# Lorsque vous ouvrez les parenthèses après print il suffit d'ajouter f" puis d'écrire 
+# entre les guillemets votre message, puis vous y ajouterez entre accolade 
+# le nom de la variable que vous voulez afficher, ici en l'occurence, 
+# nous voulons afficher la valeur de la variable nom, donc nous mettons nom entre accolade
