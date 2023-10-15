b31236e9170579c958d54734b74a355c585a2ff3
Hamza Benhammou <haamzaa.be@gmail.com>
2023-10-15 15:40:45 +0200

    comment faire une addition

---
+++ start_4.py
@@ -0,0 +1,11 @@
+# Comment additionner, il suffit de déclarer une variable dans laquelle vous y affectez une valeur, 
+# puis vous pourrez déclarer une deuxième variable dans laquelle vous affectez une valeur
+# ensuite il faudra faire un print et entre parenthèses additionner en mentionnant le nom de vos
+# variable puis l'opérateur + entre les deux
+
+
+nombre_1 = 4
+nombre_2 = 5
+print(nombre_1 + nombre_2)
+
+# ici le résultat sera 9
