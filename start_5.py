61adc1de3a7de98372f40bb6e24c87b8ff19a4d4
Hamza Benhammou <haamzaa.be@gmail.com>
2023-10-15 15:50:29 +0200

    comment définir une variable appropriée

---
+++ start_5.py
@@ -0,0 +1,14 @@
+# Définir une variable.
+# Pour cela vous devez donner un nom en rapport avec la valeur que vous allez ajouter, exemple :
+
+objet = "montre"
+nombre = 3
+reponse = True
+
+# ici nous avons déclarer 3 variables différentes, chacune ont une valeur qui est après le signe =
+# la première à une valeur str qui correspond à string ou chaîne de caractère
+# la deuxième une valeur int pour integer qui est un nombre entier
+# la troisième une valeur bool pour boolean qui est une réponse vrai ou faux souvent utilisée pour 
+# des conditions
+
+# Vous pouvez vous referrer au start_1 pour vous rappeler des différentes classes sur python
